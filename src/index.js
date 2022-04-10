a0b0f64c63ca0d2e0a26f4ab8fda9dd6af9febcb
Nakagawa fumika <nakagawa.f@x-point-1.net>
2022-04-10 23:29:43 +0900

    タイムトラベル

--- src/index.js
+++ src/index.js
@@ -40,58 +40,59 @@ import './index.css';
 
   
   class Board extends React.Component {
+    //boardからGamesへStateのリフトアップをするために、boardから削除
     //コンストラクタを追加し、初期 state として 9 個のマス目に対応する 9 個の null 値をセット
-    constructor(props){
-      super(props);
-      this.state = {
-        squares: Array(9).fill(null),
-        //どちらのプレーヤの手番なのかを決める真偽値
-        xIsNext: true,
-      };
-    }
-    //handleClickを加える
-    handleClick(i){
-      //squares を直接変更する代わりに、.slice() を呼んで配列のコピーを作成していること
-      const squares = this.state.squares.slice();
-      // handleClick を書き換えて、ゲームの決着が既についている場合やクリックされたマス目が既に埋まっている場合に早期に return する
-      if (calculateWinner(squares) || squares[i]){
-        return;
-      }
-      //xIsNext の値を反転させるようにします。
-      squares[i] = this.state.xIsNext ? 'X' : 'O';
-      this.setState({
-        squares: squares,
-        xIsNext: !this.state.xIsNext,
-      });
-    }
+    // constructor(props){
+    //   super(props);
+    //   this.state = {
+    //     squares: Array(9).fill(null),
+    //     //どちらのプレーヤの手番なのかを決める真偽値
+    //     xIsNext: true,
+    //   };
+    // }
+    // //handleClickを加える
+    // handleClick(i){
+    //   //squares を直接変更する代わりに、.slice() を呼んで配列のコピーを作成していること
+    //   const squares = this.state.squares.slice();
+    //   // handleClick を書き換えて、ゲームの決着が既についている場合やクリックされたマス目が既に埋まっている場合に早期に return する
+    //   if (calculateWinner(squares) || squares[i]){
+    //     return;
+    //   }
+    //   //xIsNext の値を反転させるようにします。
+    //   squares[i] = this.state.xIsNext ? 'X' : 'O';
+    //   this.setState({
+    //     squares: squares,
+    //     xIsNext: !this.state.xIsNext,
+    //   });
+    // }
 
       //renderSquare メソッド内で、props として value という名前の値を Square に渡す
       //squaresという配列から値をつ読み込むように書き換えます
     renderSquare(i) {
       return (
           <Square 
-              value={this.state.squares[i]} 
+              value={this.props.squares[i]} 
               //Board から Square に関数を渡すことにして、マス目がクリックされた時に Square にその関数を呼んでもらう
-              onClick={() => this.handleClick(i)}
+              //Square の位置を onClick ハンドラに渡してどのマス目がクリックされたのかを伝えるようにします。
+              onClick={() => this.props.onClick(i)}
           />
       );
     }
   
     render() {
       //render 関数内で calculateWinner(squares) を呼び出して、いずれかのプレーヤが勝利したかどうか判定します。
-      const winner = calculateWinner(this.state.squares);
-      let status;
-      if(winner){
-        status = 'winner: ' + winner;
-      }else{
-        //どちらのプレーヤの手番なのかを表示を追加
-        status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
-      }
+      // const winner = calculateWinner(this.state.squares);
+      // let status;
+      // if(winner){
+      //   status = 'winner: ' + winner;
+      // }else{
+      //   //どちらのプレーヤの手番なのかを表示を追加
+      //   status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+      // }
       
-  
       return (
         <div>
-          <div className="status">{status}</div>
+          {/* <div className="status">{status}</div> */}
           <div className="board-row">
             {this.renderSquare(0)}
             {this.renderSquare(1)}
@@ -113,15 +114,88 @@ import './index.css';
   }
   
   class Game extends React.Component {
+    constructor(props){
+      super(props);
+      this.state = {
+        history: [{
+          squares:Array(9).fill(null),
+        }],
+        stepNumber: 0,
+        xIsNext: true,
+      };
+    }
+
+    //bordからGameにhandleClickを加える
+    handleClick(i){
+      //これにより、「時間の巻き戻し」をしてからその時点で新しい着手を起こした場合に、
+      //そこから見て「将来」にある履歴（もはや正しくなくなったもの）を確実に捨て去ることができます。
+      const history = this.state.history.slice(0, this.state.stepNumber + 1);
+      const current = history[history.length -1];
+      const squares = current.squares.slice();
+      // handleClick を書き換えて、ゲームの決着が既についている場合やクリックされたマス目が既に埋まっている場合に早期に return する
+      if (calculateWinner(squares) || squares[i]){
+        return;
+      }
+      //xIsNext の値を反転させるようにします。
+      squares[i] = this.state.xIsNext ? 'X' : 'O';
+      this.setState({
+        //concat() は元の配列をミューテートしないため、こちらを利用します。
+        history: history.concat([
+          {
+          squares: squares,
+          }
+        ]),
+        // stepNumber は現在ユーザに見せている着手を反映しています。新しい着手が発生した場合は
+        //this.setState の引数の一部として stepNumber: history.length を加えることで、stepNumber を更新する必要があります。
+        stepNumber: history.length,
+        xIsNext: !this.state.xIsNext,
+      });
+    }
+    //jumpTo メソッドを定義してその stepNumber が更新されるようにします。
+    //また更新しようとしている stepNumber の値が偶数だった場合は xIsNext を true に設定します。
+    // setState で直接指定されたプロパティのみを更新しほかの state はそのまま残す
+    jumpTo(step){
+      this.setState({
+        stepNumber: step,
+        xIsNext: (step % 2) === 0,
+      });
+    }
+
     render() {
+      //ゲームのステータステキストの決定や表示の際に最新の履歴が使われるようにします。
+      const history = this.state.history;
+      const current = history[this.state.stepNumber];
+      const winner = calculateWinner(current.squares);
+      //history 配列をループ処理する部分
+      const moves = history.map((step, move) =>{
+        const desc = move ? 
+        'Go to move #' + move :
+        'Go to game start';
+        return (
+          <li key={move}>
+            <button onClick={() => this.jumpTo(move)}>{desc}
+            </button>
+          </li>
+        )
+      }
+      )
+      let status;
+      if(winner){
+        status = 'Winner: ' + winner;
+      } else{
+        status = 'Next player: ' +(this.state.xIsNext ? 'X' : 'O');
+      }
       return (
         <div className="game">
           <div className="game-board">
-            <Board />
+            <Board 
+              squares={current.squares}
+              onClick={(i) => this.handleClick(i)}
+            />
           </div>
           <div className="game-info">
-            <div>{/* status */}</div>
-            <ol>{/* TODO */}</ol>
+            <div>{status}</div>
+            <ol>{moves}</ol>
           </div>
         </div>
       );
